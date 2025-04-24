513827f5ed6d1b04834c5be9e61965ea6546084f
ernestyouniverse <ernest.dolog@youniverse.com>
2025-04-24 19:24:04 +0000

    fix(abstract-list.args): remove unnecessary class validator

--- src/shared/rest-typeorm-query/pagination/tool/abstract-list.args.ts
+++ src/shared/rest-typeorm-query/pagination/tool/abstract-list.args.ts
@@ -1,8 +1,6 @@
-import { Max } from 'class-validator';
-import { DEFAULT_PAGE_SIZE, MAX_PAGE_SIZE } from '../typeorm/typeorm-connection.js';
+import { DEFAULT_PAGE_SIZE } from '../typeorm/typeorm-connection.js';
 
 export abstract class ListArgs {
-    @Max(MAX_PAGE_SIZE)
     first: number = DEFAULT_PAGE_SIZE;
     after?: string;
 }
